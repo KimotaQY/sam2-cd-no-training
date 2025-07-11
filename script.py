aecfaac5a8a069e0c67ce876aa6113430c16a118
QY-4090D <959536695@qq.com>
2025-07-11 16:15:34 +0800

    add: 新增不同的测试脚本

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 import subprocess
+import os
 
 
-if __name__ == "__main__":
+# 测试不同参数下的结果
+def test_params():
     # 替换为你的 conda.exe 实际路径（一般在 Anaconda/Scripts 下）
     conda_path = r"E:/miniconda3/Scripts/conda.exe"
 
     # 检查 conda.exe 是否存在
-    import os
-
     if not os.path.exists(conda_path):
         raise FileNotFoundError(f"conda.exe 未找到！请检查路径: {conda_path}")
 
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     iou_list = [0.3, 0.4, 0.5, 0.6, 0.7]
     mid_list = [0, 1, 2, 3]
     diff_frame_num_list = [1, -1]
-    model_obj = ["t", "s", "b+", "l"]
+    # model_obj = ["t", "s", "b+", "l"]
+    model_obj = ["l"]
     prompt_type_list = ["points", "box", "mask"]
 
     for model_type in model_obj:
@@ -53,3 +54,143 @@ if __name__ == "__main__":
 
                         result = subprocess.run(cmd, capture_output=True, text=True)
                         print(result.stdout)
+
+
+# 测试不同模型生成的prompt效果
+def test_prompt():
+    # 替换为你的 conda.exe 实际路径（一般在 Anaconda/Scripts 下）
+    conda_path = r"E:/miniconda3/Scripts/conda.exe"
+
+    # 检查 conda.exe 是否存在
+    if not os.path.exists(conda_path):
+        raise FileNotFoundError(f"conda.exe 未找到！请检查路径: {conda_path}")
+
+    # 另一个conda环境的Python路径（需要替换为实际路径）
+    other_env_python = r"E:\miniconda3\envs\sam2-cd-no-training"
+    other_env_name = "sam2-cd-no-training"
+    # 要执行的Python文件所在目录和模块名
+    script_dir = r"E:\CD_projects\sam2-cd-no-training"
+    script_path = r"E:\CD_projects\sam2-cd-no-training\\script_eval.py"
+    module_name = "script_eval"  # 文件名（不带.py）
+
+    dataset_names = ["WHU-CD"]
+    for dataset_name in dataset_names:
+
+        test_path = f"E:/CD_datasets/{dataset_name}/test/owlv2"
+        # 获取test_path下的所有子目录，包含owlv2关键词
+        test_dirs = [
+            dir.split("_", 1)[1] for dir in os.listdir(test_path) if "owlv2" in dir
+        ]
+
+        iou_list = [0.5]
+        mid_list = [1]
+        diff_frame_num_list = [-1]
+        model_obj = ["t", "s", "b+"]
+
+        for model_type in model_obj:
+            for diff_frame_num in diff_frame_num_list:
+                for mid_frame in mid_list:
+                    for iou in iou_list:
+                        for test_dir in test_dirs:
+                            T1 = f"E:/CD_datasets/{dataset_name}/test/A"
+                            T2 = f"E:/CD_datasets/{dataset_name}/test/B"
+                            diff_label_dir = f"E:/CD_datasets/{dataset_name}/test/label"
+                            T1_label = f"E:/CD_datasets/{dataset_name}/test/owlv2/A_{test_dir}/result"
+                            T2_label = f"E:/CD_datasets/{dataset_name}/test/owlv2/B_{test_dir}/result"
+
+                            # 要调用的函数名和参数
+                            function_name = "test"
+                            args = [
+                                model_type,
+                                mid_frame,
+                                diff_frame_num,
+                                iou,
+                                T1,
+                                T2,
+                                diff_label_dir,
+                                T1_label,
+                                T2_label,
+                                dataset_name,
+                            ]
+
+                            # 使用 conda run 执行目标环境的 Python
+                            cmd = [
+                                conda_path,
+                                "run",
+                                "-n",
+                                other_env_name,
+                                "python",
+                                "-c",
+                                f"import sys; sys.path.append(r'{script_dir}'); from {module_name} import {function_name}; {function_name}(*{args})",
+                            ]
+
+                            result = subprocess.run(cmd, capture_output=True, text=True)
+                            print(result.stdout)
+
+
+# 测试不同参数的时间和内存开销
+def test_time_memory_cost():
+    # 替换为你的 conda.exe 实际路径（一般在 Anaconda/Scripts 下）
+    conda_path = r"E:/miniconda3/Scripts/conda.exe"
+
+    # 检查 conda.exe 是否存在
+    if not os.path.exists(conda_path):
+        raise FileNotFoundError(f"conda.exe 未找到！请检查路径: {conda_path}")
+
+    # 另一个conda环境的Python路径（需要替换为实际路径）
+    other_env_name = "sam2-cd-no-training"
+    # 要执行的Python文件所在目录和模块名
+    script_dir = r"E:\CD_projects\sam2-cd-no-training"
+    module_name = "test_6"  # 文件名（不带.py）
+
+    iou_list = [0.3, 0.4, 0.5, 0.6, 0.7]
+    mid_list = [0, 1, 2, 3]
+    diff_frame_num_list = [1, -1]
+    model_types = ["t", "s", "b+", "l"]
+    prompt_types = ["points", "box", "mask"]
+    obj_nums = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+    count = 0
+    for model_type in model_types:
+        for diff_frame_num in [-1]:
+            for mid_frame in [1]:
+                for iou in [0.5]:
+                    for prompt_type in ["box"]:
+                        for obj_num in [1, 100]:
+                            for _ in range(9):
+                                # 要调用的函数名和参数
+                                function_name = "main"
+                                args = [
+                                    model_type,
+                                    mid_frame,
+                                    diff_frame_num,
+                                    iou,
+                                    prompt_type,
+                                    "whu",
+                                    obj_num,
+                                ]
+
+                                # 使用 conda run 执行目标环境的 Python
+                                cmd = [
+                                    conda_path,
+                                    "run",
+                                    "-n",
+                                    other_env_name,
+                                    "python",
+                                    "-c",
+                                    f"import sys; sys.path.append(r'{script_dir}'); from {module_name} import {function_name}; {function_name}(*{args})",
+                                ]
+
+                                result = subprocess.run(
+                                    cmd, capture_output=True, text=True
+                                )
+                                print(result.stdout)
+
+                                count += 1
+                                print(f"已执行第 {count} / 72 个任务")
+
+
+if __name__ == "__main__":
+    # test_params()
+    # test_prompt()
+    test_time_memory_cost()
--- script_eval.py
+++ script_eval.py
@@ -288,40 +288,191 @@ def main(
             print("列表为空，无法计算平均值")
 
 
+def test(
+    model_type,
+    mid_frame,
+    diff_frame_num,
+    iou_threshold,
+    T1,
+    T2,
+    diff_label_dir,
+    T1_label,
+    T2_label,
+    dataset_name,
+    label_type="levir",
+    prompt_type="box",
+):
+    model_obj = {
+        "t": {
+            "checkpoint": "sam2.1_hiera_tiny.pt",
+            "config": "sam2.1_hiera_t.yaml",
+        },
+        "s": {
+            "checkpoint": "sam2.1_hiera_small.pt",
+            "config": "sam2.1_hiera_s.yaml",
+        },
+        "b+": {
+            "checkpoint": "sam2.1_hiera_base_plus.pt",
+            "config": "sam2.1_hiera_b+.yaml",
+        },
+        "l": {
+            "checkpoint": "sam2.1_hiera_large.pt",
+            "config": "sam2.1_hiera_l.yaml",
+        },
+    }
+    checkpoint = model_obj[model_type]["checkpoint"]
+    config = model_obj[model_type]["config"]
+    # 加载SAM2 video predictor
+    sam2_checkpoint = os.path.join("E:/CD_Checkpoints", checkpoint)
+    model_cfg = os.path.join(
+        "E:/CD_projects/sam2-cd-no-training/sam2/configs/sam2.1", config
+    )
+
+    # 输入前后时相图片
+    # T1 = kwargs.get("T1", None)
+    # T2 = kwargs.get("T2", None)
+    # diff_label_dir = kwargs.get("diff_label_dir", None)
+    # T1_label = kwargs.get("T1_label", None)
+    # T2_label = kwargs.get("T2_label", None)
+
+    if None in [T1, T2, diff_label_dir, T1_label, T2_label]:
+        print("请输入前后时相图片路径和标签路径")
+        return
+
+    # 读取前后时相路径中的所有文件名
+    img_names = [p for p in os.listdir(T1) if os.path.splitext(p)[-1] in [".png"]]
+
+    # 获取prompt的文件夹名称
+    prompt_folder_name = os.path.normpath(T2_label).split(os.sep)[-2]
+    prompt_folder_name = prompt_folder_name.split("_", 1)[1]
+
+    output_dir = f"./logs/{dataset_name}/generate_{model_type}_{prompt_type}_mid{mid_frame}_{diff_frame_num}_iou{iou_threshold}/{prompt_folder_name}"
+
+    # 存在的文件夹则跳过
+    if os.path.isdir(output_dir):
+        print(f"{output_dir} 已存在")
+        return
+    else:
+        os.makedirs(output_dir, exist_ok=True)
+
+    with open(os.path.join(output_dir, "log.txt"), "w", encoding="utf-8") as f:
+        F1_meter = AverageMeter()
+        IoU_meter = AverageMeter()
+        Acc_meter = AverageMeter()
+        Pre_meter = AverageMeter()
+        Rec_meter = AverageMeter()
+
+        for idx, img_name in enumerate(img_names):
+            predictor = build_sam2_video_predictor(
+                model_cfg, sam2_checkpoint, device=device
+            )
+
+            diff_mask_list = step_one(
+                img_name,
+                T1,
+                T2,
+                T1_label,
+                T2_label,
+                predictor,
+                label_type=label_type,
+                mid_frame=mid_frame,
+                diff_frame_num=diff_frame_num,
+                iou_threshold=iou_threshold,
+                prompt_type=prompt_type,
+                # prompts=prompts,
+            )
+
+            diff_mask = sum_masks_dict(*diff_mask_list, iou_threshold=iou_threshold)
+
+            # 读取标签图（单通道）
+            label_mask = cv2.imread(
+                os.path.join(diff_label_dir, img_name), cv2.IMREAD_GRAYSCALE
+            )
+            # iou = compute_mask_iou(diff_mask, label_mask)
+            acc, precision, recall, f1, iou = binary_accuracy(diff_mask, label_mask)
+
+            F1_meter.update(f1)
+            Acc_meter.update(acc)
+            IoU_meter.update(iou)
+            Pre_meter.update(precision)
+            Rec_meter.update(recall)
+
+            print(
+                f"{idx+1}/{len(img_names)} iou: {iou} f1: {f1} pre: {precision} rec: {recall} acc:{format(acc*100,'.2f')}"
+            )
+            f.write(
+                f"{idx+1}/{len(img_names)} f1: {format(f1*100,'.2f')} iou: {format(iou*100,'.2f')} pre: {format(precision*100,'.2f')} rec: {format(recall*100,'.2f')} acc:{format(acc*100,'.2f')} name: {img_name}\n"
+            )
+
+            # 保存mask
+            h, w = diff_mask.shape[-2:]
+            mask_image = diff_mask.reshape(h, w, 1)
+            cv2.imwrite(os.path.join(output_dir, img_name), mask_image * 255)
+
+            if "predictor" in locals():
+                del predictor
+            torch.cuda.empty_cache()
+
+        try:
+            print(
+                f"平均值 iou: {IoU_meter.avg} f1: {F1_meter.avg} pre: {Pre_meter.avg} rec: {Rec_meter.avg} acc:{Acc_meter.avg}"
+            )
+            f.write(
+                f"平均值 iou: {IoU_meter.avg} f1: {F1_meter.avg} pre: {Pre_meter.avg} rec: {Rec_meter.avg} acc:{Acc_meter.avg}"
+            )
+        except statistics.StatisticsError:
+            print("列表为空，无法计算平均值")
+
+
+# if __name__ == "__main__":
+#     # 读取文件夹中所有子文件夹的名称
+#     input_dir = "E:/CD_datasets/WHU-CD/test"
+#     exclude_folders = ["A", "B", "label"]
+#     folder_names = [
+#         name
+#         for name in os.listdir(input_dir)
+#         if os.path.isdir(os.path.join(input_dir, name)) and name not in exclude_folders
+#     ]
+#     # 将文件夹名称去掉第一个_之后的所有字符进行保存
+#     folder_names = [name[name.index("_") + 1 :] for name in folder_names]
+#     # 去除掉掉重复的文件夹名称
+#     folder_names = list(set(folder_names))
+#     print(folder_names)
+
+#     model_type = "l"  # SAM2参数
+#     mid_frame = 1  # 插帧数
+#     prompt_type = "mask"  # 提示类型
+
+#     T1 = "E:/CD_datasets/WHU-CD//test/A"
+#     T2 = "E:/CD_datasets/WHU-CD//test/B"
+#     diff_label_dir = "E:/CD_datasets/WHU-CD/test/label"
+#     T1_label = "E:/CD_datasets/WHU-CD//before_label"
+#     T2_label = "E:/CD_datasets/WHU-CD//after_label"
+#     main(
+#         model_type,
+#         mid_frame,
+#         -1,
+#         0.5,
+#         prompt_type,
+#         label_type="whu",
+#         T1=T1,
+#         T2=T2,
+#         T1_label=T1_label,
+#         T2_label=T2_label,
+#         diff_label_dir=diff_label_dir,
+#     )
+
 if __name__ == "__main__":
-    # 读取文件夹中所有子文件夹的名称
-    input_dir = "E:/CD_datasets/WHU-CD/test"
-    exclude_folders = ["A", "B", "label"]
-    folder_names = [
-        name
-        for name in os.listdir(input_dir)
-        if os.path.isdir(os.path.join(input_dir, name)) and name not in exclude_folders
-    ]
-    # 将文件夹名称去掉第一个_之后的所有字符进行保存
-    folder_names = [name[name.index("_") + 1 :] for name in folder_names]
-    # 去除掉掉重复的文件夹名称
-    folder_names = list(set(folder_names))
-    print(folder_names)
-
-    model_type = "l"  # SAM2参数
-    mid_frame = 1  # 插帧数
-    prompt_type = "mask"  # 提示类型
-
-    T1 = "E:/CD_datasets/WHU-CD//test/A"
-    T2 = "E:/CD_datasets/WHU-CD//test/B"
-    diff_label_dir = "E:/CD_datasets/WHU-CD/test/label"
-    T1_label = "E:/CD_datasets/WHU-CD//before_label"
-    T2_label = "E:/CD_datasets/WHU-CD//after_label"
-    main(
-        model_type,
-        mid_frame,
+    test(
+        "b+",
+        1,
         -1,
         0.5,
-        prompt_type,
-        label_type="whu",
-        T1=T1,
-        T2=T2,
-        T1_label=T1_label,
-        T2_label=T2_label,
-        diff_label_dir=diff_label_dir,
+        "E:/CD_datasets/LEVIR-CD/test/A",
+        "E:/CD_datasets/LEVIR-CD/test/B",
+        "E:/CD_datasets/LEVIR-CD/test/label",
+        "E:/CD_datasets/LEVIR-CD/test/owlv2/A_owlv2_large_[roof]/result",
+        "E:/CD_datasets/LEVIR-CD/test/owlv2/B_owlv2_large_[roof]/result",
+        "LEVIR-CD",
+        "box",
     )
